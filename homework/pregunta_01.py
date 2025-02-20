90b79d9ce5d9ee3b74193015af72f3981e0935be
SrRobls <joroblesr@unal.edu.co>
2025-02-20 09:06:21 -0500

    Remove setup script and add VSCode settings; implement data cleaning in pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,6 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en la pregunta.
-"""
-
+import pandas as pd
+import os
+import re
 
 def pregunta_01():
     """
@@ -11,5 +10,48 @@ def pregunta_01():
     realizar la limpieza de los datos.
 
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
-
     """
+
+    archivo_entrada = 'files/input/solicitudes_de_credito.csv'
+    archivo_salida = 'files/output/solicitudes_de_credito.csv'
+
+    df = pd.read_csv(archivo_entrada, sep=';', index_col=0)
+
+    df['sexo'] = df['sexo'].str.lower()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().str.strip()
+    
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].str.replace('_', ' ', regex=False).str.replace('-', ' ', regex=False)
+
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_', ' ', regex=False).str.replace('-', ' ', regex=False).str.strip()
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip()
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '', regex=False)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '', regex=False)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('.00', '', regex=False)
+    df['monto_del_credito'] = pd.to_numeric(df['monto_del_credito'], errors='coerce')
+
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.replace('_', ' ').str.replace('-', ' ').str.strip()
+
+
+    df["fecha_de_beneficio"] = pd.to_datetime(
+            df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+        ).combine_first(pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce"))
+
+
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    df=df.drop_duplicates()
+    df.dropna(inplace=True)
+
+
+    output_directory = os.path.dirname(archivo_salida)
+    os.makedirs(output_directory, exist_ok=True)
+    df.to_csv(archivo_salida, sep=';', index=False, encoding="utf-8")
+
+    print("✅ Archivo limpio guardado en:", archivo_salida)
+
+
+pregunta_01()
